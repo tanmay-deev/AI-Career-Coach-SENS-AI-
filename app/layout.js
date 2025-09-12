9e439b057899e2f8a818408fee77136f2ffe37ab
tanmay-deev <tanmaybonde10@gmail.com>
2025-09-13 00:30:02 +0530

    All file added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +19,29 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* Header */}
+
+          <main className="min-h-screen">{children}</main>
+
+          {/* Footer */}
+
+          <footer className="bg-muted/50 py-10">
+            <div className="container">
+              <p>Made with love by Tanmay</p>
+            </div>
+          </footer>
+
+        </ThemeProvider>
       </body>
     </html>
   );
