6bfbd59856e6e516bbbcb13f706564e4b08222f9
tanmay-deev <tanmaybonde10@gmail.com>
2025-09-13 13:09:51 +0530

    Clerk setup done

---
+++ components/header.jsx
@@ -0,0 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import React from 'react'
+
+const Header = () => {
+    return ( <div>
+        <SignedOut>
+            <SignInButton />
+        </SignedOut>
+        <SignedIn>
+            <UserButton />
+        </SignedIn>
+    </div> );
+};
+
+export default Header;
