2785f1e9053eb4f1f1773d1394071f09d55ec0ae
tanmay-deev <tanmaybonde10@gmail.com>
2025-09-13 19:10:06 +0530

    Authentication Page done

--- app/page.js
+++ app/page.js
@@ -8,13 +8,7 @@ export default function Home() {
   
   return (
    <div>
-    <h1>Hello everyone</h1>
-    <div>
-
-    </div>
-
-    <br/>
-    <Button>Hello</Button>
+    
 
    </div>
   );
--- components/header.jsx
+++ components/header.jsx
@@ -3,37 +3,86 @@ import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react'
 import { Button } from './ui/button';
-import { LayoutDashboard } from 'lucide-react';
+import { ChevronDown, FileText, FileTextIcon, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
 
 const Header = () => {
     return (
         <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
 
-            <nav className='container mx-auto flex px-4 pt-4 h-16 intem-center justify-between'>
+            <nav className='container mx-auto flex px-4 h-16 items-center justify-between'>
                 <Link href='/' >
                     <Image src="/logo.png" alt="Logo" width={200} height={60}
 
                         className="h-12 py-1 w-auto object-contain" />
                 </Link>
 
-                <div>
+                <div className='flex items-center space-x-2 md:space-x-4'>
                     <SignedIn>
                         <Link href={"/dashboard"}>
-                            <Button>
+                            <Button variant = "outline">
                                 <LayoutDashboard className='h-4 w-4 ' />
-                                Industry Insights
+                                <span className='hidden md:block'>
+                                    Industry Insights
+                                </span>
                             </Button>
                         </Link>
+                    
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button >
+                                <StarsIcon className='h-4 w-4 ' />
+                                <span className='hidden md:block'>
+                                    Growth Tools
+                                </span>
+                                <ChevronDown className='h-4 w-4' />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem>
+                                <Link href={"/resume"} className='flex items-center gap-2'>
+                                    <FileText className='h-4 w-4 ' />
+                                    <span>Resume Builder</span>
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link href={"/ai-cover-letter"} className='flex items-center gap-2'>
+                                    <PenBox className='h-4 w-4 ' />
+                                    <span>Cover Letter</span>
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link href={"/interview"} className='flex items-center gap-2'>
+                                    <GraduationCap className='h-4 w-4 ' />
+                                    <span>Interview Prep</span>
+                                </Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton>
+                            <Button variant="outline">Sign In</Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton 
+                        appearance={{
+                            elements: {
+                                avatarBox: "w-16 h-16",
+                                // avatarImage: "w-16 h-16",
+                                userButtonPopoverCard: "shadow-xl",
+                                userPreviewMainIdentifier: "font-semibold",
+                            },
+                        }}
+                        afterSignOutUrl='/'
+                        
+                        />
                     </SignedIn>
                 </div>
             </nav>
 
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
         </header>
     );
 };
